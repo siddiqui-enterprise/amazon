8df7a2826e8a1b8f21eff10d561875252adbd707
mohammadaamir1711 <mohammad.aamir.1711@gmail.com>
2021-11-05 05:17:48 -0400

    changes in firstjava

--- firstjava.java
+++ firstjava.java
@@ -3,5 +3,16 @@ class hello
 public static void main(String args[])
 {
 System.out.print{"hello to all");
+test t1= new test();
+t1.func();
+
 }
 }
+class test
+{
+void func()
+{
+System.out.print("this is for test");
+}
+}
+
